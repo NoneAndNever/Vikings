4c4936b06c9e175e30a1063af3b8082268bbd0d0
NoneAndNever <43804135+NoneAndNever@users.noreply.github.com>
2023-07-24 21:36:15 +0800

    Initial commit

---
+++ Source/Vikings/Vikings.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Vikings.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Vikings, "Vikings" );
---
+++ Source/Vikings/VikingsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VikingsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIKINGS_API AVikingsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
